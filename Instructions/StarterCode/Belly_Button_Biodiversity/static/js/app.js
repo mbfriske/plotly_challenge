887e27e2e0f74a315b9616a40b680c5b614bd83e
Matthew Friske <MattF@Matthews-MacBook-Pro.local>
2019-09-21 12:25:21 -0500

    adding finalized files.

--- Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
+++ Instructions/StarterCode/Belly_Button_Biodiversity/static/js/app.js
@@ -3,13 +3,22 @@ function buildMetadata(sample) {
   // @TODO: Complete the following function that builds the metadata panel
 
   // Use `d3.json` to fetch the metadata for a sample
+  d3.json(`/metadata/${sample}`).then(function(sampleMetadata) {
+    console.log(sampleMetadata);
+
     // Use d3 to select the panel with id of `#sample-metadata`
+      var dataSelect = d3.select("#sample-metadata");
 
     // Use `.html("") to clear any existing metadata
+      dataSelect.html("");
 
     // Use `Object.entries` to add each key and value pair to the panel
     // Hint: Inside the loop, you will need to use d3 to append new
     // tags for each key-value in the metadata.
+      Object.entries(sampleMetadata).forEach(([key, value]) => {
+        dataSelect.append('p').text(`${key}: ${value}`);
+      })
+    })
 
     // BONUS: Build the Gauge Chart
     // buildGauge(data.WFREQ);
@@ -18,8 +27,41 @@ function buildMetadata(sample) {
 function buildCharts(sample) {
 
   // @TODO: Use `d3.json` to fetch the sample data for the plots
+  d3.json(`/samples/${sample}`).then(function(samplePie) {
+    console.log(samplePie);
+
+   var out_ids = samplePie.otu_ids;
+   var sample_values = samplePie.sample_values;
+   var otu_labels = samplePie.otu_labels;
+
+   var trace1 = [{
+     labels: out_ids.slice(0, 10),
+     values: sample_values.slice(0, 10),
+     hovertext: otu_labels.slice(0, 10),
+     type: 'pie'
+   }];
+   var layout = {
+     title: "Belly Button Pie Chart",
+   };
+   Plotly.newPlot("pie", trace1, layout);
 
     // @TODO: Build a Bubble Chart using the sample data
+    var trace2 = [{
+      x: out_ids,
+      y: sample_values,
+      mode: 'markers',
+      marker: {
+        size: sample_values,
+        color: out_ids,
+        colorscale: 'light blue'
+      }
+    }];
+    var layout2 = {
+      title: "<b>Belly Button Bubble Chart</b>",
+    };
+    Plotly.newPlot("bubble", trace2, layout2);
+  })
+
 
     // @TODO: Build a Pie Chart
     // HINT: You will need to use slice() to grab the top 10 sample_values,
